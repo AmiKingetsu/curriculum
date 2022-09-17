f54b430ae267d19b5b55d71e0aacfd1f54b3f83e
AmiKingetsu <111890346+AmiKingetsu@users.noreply.github.com>
2022-09-18 00:46:06 +0900

    課題提出テスト
    データ型・文字列クラスの課題

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,12 @@
+public class Task1_1 {
+    public static void main(String[] args){
+        int hogeInt = 50;
+    String hogeString = "テスト";
+    double hogeDouble = 0.2;
+hogeInt = 100;
+hogeString = "合格";
+System.out.println(hogeInt);
+System.out.println(hogeString);
+System.out.println(hogeDouble);
+}
+}
