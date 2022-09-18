25e259050dfea2a3ce60de7f9845c49f8a79fdb9
AmiKingetsu <111890346+AmiKingetsu@users.noreply.github.com>
2022-09-18 12:57:11 +0900

    課題提出テスト
    配列の課題

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,15 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+    String [] country = {"JAPAN","AMERICA","KOREA"};
+    System.out.println(country.length);
+    
+
+    String [] strArray = {"りんご","もも","ぶどう"};
+    System.out.println(strArray[1]);
+
+    int[] intArray = { 10, 20, 30, 40, 50 };
+    System.out.println(intArray[1] + intArray[4]);
+
+    
+    }
+}
