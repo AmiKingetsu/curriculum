d34443cbf8a99f3fd4fd04ec38894b3c58184fad
AmiKingetsu <111890346+AmiKingetsu@users.noreply.github.com>
2022-09-25 16:44:45 +0900

    課題提出テスト
    ループ文課題

---
+++ JavaStudy/1-5/Task1_5.java
@@ -0,0 +1,27 @@
+public class Task1_5 {
+
+    public static void main(String[] args) {
+        String[]fruits={"みかん","りんご","ぶどう","メロン"};
+        for ( int i=0; i<fruits.length; i++){
+            System.out.println(fruits[i]); 
+    
+        }
+        int i = 1;
+        while(i <= 100) {
+          System.out.print(i);
+          i++;
+        }
+        System.out.println();
+        for ( int u = 1; u <= 9; u ++ ) {
+  			for ( int j = 1; j <= 9; j ++ ) {
+   				System.out.print( "|" + u * j );
+            }
+
+            System.out.println();
+
+
+    
+    }
+    
+}
+    }
