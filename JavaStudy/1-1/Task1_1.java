df7b7e2c170d596f787f1a07794adc955820ed48
AmiKingetsu <111890346+AmiKingetsu@users.noreply.github.com>
2023-02-27 15:23:03 +0900

    Update Task1_1.java

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -1,7 +1,7 @@
 public class Task1_1 {
     public static void main(String[] args){
         int hogeInt = 50;
-    String hogeString = "テスト";
+    String hogeString = "テスト!!";
     double hogeDouble = 0.2;
 hogeInt = 100;
 hogeString = "合格";
